e78ab8cae446563e5191a08138ae21990ec16c3a
Dilshan Ariyawansha <it21173790@my.sliit.lk>
2025-05-27 21:40:24 +0530

    new deployment

--- vocabulary_gamified/src/pages/EnglishLearningHomepage.jsx
+++ vocabulary_gamified/src/pages/EnglishLearningHomepage.jsx
@@ -418,7 +418,7 @@ const EnglishLearningHomepage = () => {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 EnglishMaster. All rights reserved.</p>
+            <p>&copy; 2025 Readify. All rights reserved.</p>
           </div>
         </div>
       </footer>
